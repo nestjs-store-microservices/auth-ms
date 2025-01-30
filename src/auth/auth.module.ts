dd6bbd1ca25149b09d4fc2fcc266badb498e59e9
lgcholoquinga <luisg.choloquinga@gamil.com>
2025-01-29 21:52:39 -0500

    modify token life

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { envs } from 'src/config';
     JwtModule.register({
       global: true,
       secret: envs.jwtSecret,
-      signOptions: { expiresIn: '10s' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
 })
